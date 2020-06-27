1f25c1e94efdedb91c2709588e9619b7245ca3a9
liuzhaocai <liuzhaocai@beyondcent.com>
2020-06-27 15:27:29 +0800

    add currying.js

---
+++ 手写代码/currying.js
@@ -0,0 +1,14 @@
+function currying(fn,...args){
+  if(fn.length <= args.length){
+      return fn(...args)
+  }
+  return function(...args1){
+      return currying(fn,...args,...args1)
+  }
+}
+function add(a,b,c){
+  return a + b + c
+}
+add(1,2,3) // 6
+var curryingAdd = currying(add);
+curryingAdd(1)(2)(3) // 6
